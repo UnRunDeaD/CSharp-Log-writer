3c8b9b974add66c9a91025ffa5269119d89e9612
Pascal <unrundead_designs@gmx.de>
2018-05-18 23:34:15 +0200

    Edit Comment from Code

--- CSharpLogWriter/CSharpLogWriter/Program.cs
+++ CSharpLogWriter/CSharpLogWriter/Program.cs
@@ -13,7 +13,6 @@ namespace CSharpLogWriter
             //here are all examples for every Function how is possible for the Log at this point
             //you can easily expand the Log with your own conditions
 
-            //some easy code
             Log.Developer(function: "YourFunction", "Your Log Message or Exception", level: "important");
 
             Log.Error(function: "YourFunction", "Your Log Message or Exception", level: "priority");
